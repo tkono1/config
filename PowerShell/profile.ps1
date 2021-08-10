c7cb843734c44fc83f9bd627b58c0c56554a573c
tkono1 <tkono1@gmail.com>
2021-08-10 18:12:19 +0900

    Added new prompt for posh-git.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -19,16 +19,28 @@ if ($host.Name -eq 'ConsoleHost') {
 ## Defining functions ##
 
 function prompt () {
-    [string]$isAdmin = '>'
-    [string]$PromptColor1 = 'DarkGreen'
-    [string]$PromptColor2 = 'DarkCyan'
+        [string]$isAdmin = '>'
+        [string]$PromptColor1 = 'DarkGreen'
+        [string]$PromptColor2 = 'DarkCyan'
 
-    Write-Host ("[$(($env:USERNAME).ToLower())@$(($env:COMPUTERNAME).ToLower()):") -ForegroundColor $PromptColor1 -NoNewline
-    Write-Host ((Get-Location).Path).Replace($HOME, '~') -ForegroundColor $PromptColor2 -NoNewline
-    #Write-Host ((Get-Location)) -ForegroundColor $PromptColor2 -NoNewline
-    & Write-VcsStatus
-    Write-Host ("]" + $isAdmin) -ForegroundColor $PromptColor1 -NoNewline
-    Return " "
+    if (Get-Module -Name 'posh-git') {
+        $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+        $GitPromptSettings.DefaultPromptPrefix.Text = "[$(($env:USERNAME).ToLower())@$(($env:COMPUTERNAME).ToLower())]"
+        $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::${PromptColor1}
+        $GitPromptSettings.DefaultPromptPath.ForegroundColor = [ConsoleColor]::${PromptColor2}
+        $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::${PromptColor1}
+
+        & $GitPromptScriptBlock
+    } else {
+        [string]$isAdmin = '>'
+        [string]$PromptColor1 = 'DarkGreen'
+        [string]$PromptColor2 = 'DarkCyan'
+
+        Write-Host ("[$(($env:USERNAME).ToLower())@$(($env:COMPUTERNAME).ToLower()):") -ForegroundColor $PromptColor1 -NoNewline
+        Write-Host ((Get-Location).Path).Replace($HOME, '~') -ForegroundColor $PromptColor2 -NoNewline
+        Write-Host ("]" + $isAdmin) -ForegroundColor $PromptColor1 -NoNewline
+        Return " "
+    }
 }
 
 function which {
