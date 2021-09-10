6f90780b2ef70d3b108aa1040f08d1eab5adcd4c
tkono1 <tkono1@gmail.com>
2021-09-11 00:38:59 +0900

    Update whole configurations.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,28 +1,49 @@
+;;
 ;; Language and coding system.
-(prefer-coding-system 'utf-8)
+;;
+;; Set character code.
 ;(set-language-environment 'Japanese)
 (set-locale-environment "en_US.UTF-8")
+(prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
+;;
+;; }}
+;;
 
-;; Emacs version detection.
-(defun x->bool (elt) (not (not elt)))
-(setq emacs25-p (string-match "^25" emacs-version)
-  emacs26-p (string-match "^26" emacs-version)
-  emacs27-p (string-match "^27" emacs-version))
+;;
+;; Backup files {{
+;;
+;; Don't create backup file.
+(setq make-backup-files nil)
+(setq backup-inhibited t)
 
-;; System type detection.
-(setq darwin-p (eq system-type 'darwin)
-  ns-p (eq window-system 'ns)
-  linux-p (eq system-type 'gnu/linux)
-  nt-p (eq system-type 'windows-nt))
+;; Don't create auto save file.
+(setq auto-save-default nil)
+(setq auto-save-list-file-prefix nil)
 
-;; Key mapping.
+;; Delete auto save file when quit.
+(setq delete-auto-save-files t)
+;;
+;; }}
+;;
+
+;;
+;; Key binding {{
+;;
+;; Enable C-h deletes one character.
 (global-set-key "\C-h" 'delete-backward-char)
+(keyboard-translate ?\C-h ?\C-?)
+(global-set-key "\C-h" nil)
 
-;; Disable startup message.
-(setq inhibit-startup-screen t)
+;; C-k deletes whole line.
+(setq kill-hole-line t)
+
+;; Set undo.
+(global-set-key "\C-z" 'undo)
 
 ;; Tab settings.
 (setq-default tab-width 4)
@@ -31,20 +52,39 @@
                     68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 (setq-default indent-tabs-mode nil)
 
-;; Don't create backup file.
-(setq backup-inhibited t)
-;; Delete auto save file when quit.
-(setq delete-auto-save-files t)
+;; Replace "yes or no" to "y or n".
+(fset 'yes-or-no-p 'y-or-n-p)
+;;
+;; }}
+;;
 
-;; Disable toolbar.
-(when window-system
-  (tool-bar-mode -1))
+;;
+;; Visualize {{
+;;
+;; Disable startup message.
+(setq inhibit-startup-screen 1)
+
+;; Disable initial scratch  message.
+(setq initial-scratch-message "")
+(setq initial-scratch-message nil)
+
+;; Disable error beep.
+(setq inhibit-startup-message t)
+
+;; Disable toolbar in Window mode.
+(if window-system
+    (tool-bar-mode -1))
+
+;; Disable menubar.
+(menu-bar-mode -1)
 
 ;; Show line number.
-(when emacs26-p
-  (global-linum-mode t))
-
-;; Share clipboard with X.
-(when linux-p
-  (when window-system
-    (setq x-select-enable-clipboard t)))
+(require 'linum)
+(global-linum-mode 1)
+(setq linum-format "%4d")
+(set-face-attribute 'linum nil
+    :foreground "#a9a9a9"
+    :background "#404040")
+;;
+;; }}
+;;
