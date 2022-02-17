7ca9838118abdabecd5b1a42deafeca0dacf9787
tkono1 <tkono1@gmail.com>
2022-02-17 20:54:27 +0900

    Remove t_8f/t_8b used for tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -53,10 +53,6 @@ if vim.fn.has('termguicolors') and vim.env.COLORTERM == 'truecolor' then
     vim.opt.termguicolors = true
     vim.opt.cursorline = true
     --vim.opt.list =  tue
-    if vim.env.TMUX ~= nill then
-        vim.cmd [[let &t_8f="\<Esc>[38;2;%lu;%lu;%lum"]]
-        vim.cmd [[let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"]]
-    end
 end
 
 -- Set background scheme. Set this option before syntax highlighting.
