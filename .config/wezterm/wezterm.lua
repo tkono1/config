95afa80012abe57bf01022b2ee9cab7cf65090f4
tkono1 <tkono1@gmail.com>
2024-08-18 16:12:39 +0900

    Remove old config line.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,34 +2,28 @@
 local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
-local config = {}
+local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
-
--- Set color scheme.
+-- Set color scheme
 config.color_scheme = 'nord'
 
 config.window_close_confirmation = 'NeverPrompt'
 
---- Font settings. {{
+--- Font settings {{
 config.font = wezterm.font({ family = 'Menlo', weight = 'Regular' })
 config.font_size = 12
 --- }}
 
---- Window settings. {{
+--- Window settings {{
 config.window_background_opacity = 0.98
 config.initial_cols = 80
 config.initial_rows = 60
 
--- enable scroll bar.
+-- enable scroll bar
 -- config.enable_scroll_bar = true
 --- }}
 
---- Tab settings. {{
+--- Tab settings {{
 config.use_fancy_tab_bar = true
 --config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW'
 config.window_decorations = 'TITLE|RESIZE|MACOS_FORCE_ENABLE_SHADOW'
@@ -37,13 +31,13 @@ config.window_frame = {
     font = wezterm.font({ family = 'Menlo', weight = 'Bold'}),
     font_size = 12,
 
-    -- Background color of tab bar.
+    -- Background color of tab bar
     active_titlebar_bg = '#20232e',
     inactive_titlebar_bg = '#20232e',
 }
 --- }}
 
---- Color settings. {{
+--- Color settings {{
 config.colors = {
     tab_bar = {
         inactive_tab_edge = '#2f333e',
