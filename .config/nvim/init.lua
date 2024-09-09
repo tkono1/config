94e3ec19227a21ccb21b467ebf454669b1244c39
tkono1 <tkono1@gmail.com>
2024-09-09 10:31:29 +0900

    Add Lazy autoupdate function.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -251,3 +251,17 @@ vim.opt.laststatus = 2
 -- Hide current mode if lualine.nvim is set.
 vim.opt.showmode = false
 --- }}
+
+-- Lazy autoupdate.
+local function augroup(name)
+    return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = augroup("autoupdate"),
+    callback = function()
+        if require("lazy.status").has_updates then
+            require("lazy").update({ show = false, })
+        end
+    end,
+})
