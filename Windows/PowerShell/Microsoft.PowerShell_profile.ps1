d9e8e5554074e0be70e9dbdbac0ab82391406b78
tkono1 <tkono1@gmail.com>
2025-03-06 16:02:23 +0900

    Shrink code.

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -20,13 +20,28 @@ if ($host.Name -eq 'ConsoleHost') {
     }
 }
 
+[string]$CWD = (Get-Location).Path
+
+if (Get-Module -Name 'posh-git') {
+    [string]$RgbPromptColor1 = '0x00BFFF' # DeepSkyBlue
+    [string]$RgbPromptColor2 = '0x40E0D0' # Turquoise
+
+    $GitPromptSettings.DefaultPromptPrefix.Text = "PS "
+    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = $RgbPromptColor1
+    $GitPromptSettings.DefaultPromptPath.ForegroundColor = $RgbPromptColor2
+    $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = $RgbPromptColor1
+    $GitPromptSettings.DefaultPromptSuffix.Text = "$('>' * ($nestedPromptLevel + 1)) "
+} else {
+    [string]$ESC27 = "$([char]27)"
+    $out = "${ESC27}[38;5;45m[PS :${ESC27}[0m${ESC27}[38;5;140m${CWD}]$('>' * ($nestedPromptLevel + 1))${ESC27}[0m "
+}
+
 if ($env:TERM_PROGRAM -ne 'vscode') {
     $Global:__LastHistoryId = -1
 
     function prompt {
-        [string]$Computername = ($env:COMPUTERNAME).ToLower()
-        [string]$CWD = (Get-Location).Path
         [string]$ESC07 = "$([char]07)"
+        [string]$OSC133 = "`e]133"
 
         $FakeCode = [int]!$global:?
         # NOTE: We disable strict mode for the scope of this function because it unhelpfully throws an
@@ -38,63 +53,40 @@ if ($env:TERM_PROGRAM -ne 'vscode') {
         if ($Global:__LastHistoryId -ne -1) {
             if ($LastHistoryEntry.Id -eq $Global:__LastHistoryId) {
                 # Don't provide a command line or exit code if there was no history entry (eg. ctrl+c, enter on no command)
-                $Result += "`e]133;D`a"
+                $Result += "${OSC133};D`a"
             } else {
                 # Command finished exit code
                 # OSC 133 ; D [; <ExitCode>] ST
-                $Result += "`e]133;D;${FakeCode}`a"
+                $Result += "${OSC133};D;${FakeCode}`a"
             }
         }
 
         # Prompt started
-        $Result += "`e]133;A${ESC07}"
+        $Result += "${OSC133};A${ESC07}"
         # Current Working Directory
         $Result += "`e]9;9;`"${CWD}`"${ESC07}"
-        
-        # Prompt here
-        [string]$RgbPromptColor1 = '0x00BFFF' # DeepSkyBlue
-        [string]$RgbPromptColor2 = '0x40E0D0' # Turquoise
 
         if (Get-Module -Name 'posh-git') {
-            $GitPromptSettings.DefaultPromptPrefix.Text = "PS "
-            $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = $RgbPromptColor1
-            $GitPromptSettings.DefaultPromptPath.ForegroundColor = $RgbPromptColor2
-            $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = $RgbPromptColor1
-            $GitPromptSettings.DefaultPromptSuffix.Text = "$('>' * ($nestedPromptLevel + 1)) "
             $Global:__OriginalPrompt = ${GitPromptScriptBlock}
-
             $Result += $Global:__OriginalPrompt.Invoke()
         } else {
-            [string]$ESC27 = "$([char]27)"
-
-            $Result += "${ESC27}[38;5;45m[PS :${ESC27}[0m${ESC27}[38;5;140m${CWD}]$('>' * ($nestedPromptLevel + 1))${ESC27}[0m "
+            $Result += $out
         }
 
         # Prompt ended, Command started
-        $Result += "`e]133;B${ESC07}"
+        $Result += "${OSC133};B${ESC07}"
         $Global:__LastHistoryId = $LastHistoryEntry.Id
 
         return $Result
     } 
 } elseif ($env:TERM_PROGRAM -eq 'vscode') {
-    function prompt () {
-        [string]$RgbPromptColor1 = '0x00BFFF' # DeepSkyBlue
-        [string]$RgbPromptColor2 = '0x40E0D0' # Turquoise
-    
-        if (Get-Module -Name 'posh-git') {
-            $GitPromptSettings.DefaultPromptPrefix.Text = "PS "
-            $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = $RgbPromptColor1
-            $GitPromptSettings.DefaultPromptPath.ForegroundColor = $RgbPromptColor2
-            $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = $RgbPromptColor1
-            $GitPromptSettings.DefaultPromptSuffix.Text = "$('>' * ($nestedPromptLevel + 1)) "
-    
+    function prompt () {    
+        if (Get-Module -Name 'posh-git') {    
             & $GitPromptScriptBlock
         } else {
-            [string]$ESC27 = "$([char]27)"
+            $Result = $out
 
-            $out = "${ESC27}[38;5;45m[PS :${ESC27}[0m${ESC27}[38;5;140m${CWD}]$('>' * ($nestedPromptLevel + 1))${ESC27}[0m "
-
-            return $out
+            return $Result
         }
     }
 }
