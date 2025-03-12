a541649482bc8083e1b2eeae2a9b806f86751e0c
tkono1 <tkono1@gmail.com>
2025-03-12 21:40:38 +0900

    Use hash table for PSReadLineOption command.

--- Windows/PowerShell/Microsoft.VSCode_profile.ps1
+++ Windows/PowerShell/Microsoft.VSCode_profile.ps1
@@ -1,5 +1,13 @@
 if (Get-InstalledPSResource -Name 'PSReadLine') {
     #Import-Module PSReadLine
-    Set-PSReadlineOption -EditMode Emacs
-    Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
+    $PSROptions = @{
+        EditMode = "Emacs"
+        PredictionSource = "HistoryAndPlugin"
+        PredictionViewStyle = "ListView"
+        Colors = @{
+            Operator = "`e[36m"
+            Parameter = "`e[36m"
+        }
+    }
+    Set-PSReadLineOption @PSROptions
 }
