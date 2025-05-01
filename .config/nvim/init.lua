cf2b94b46c1ed2b0803bbd8cfeab1ce31e74616c
tkono1 <tkono1@gmail.com>
2025-05-01 14:56:37 +0900

    Format indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,12 +43,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
-        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-        { out, "WarningMsg" },
-        { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
     end
 end
 vim.opt.rtp:prepend(lazypath)
